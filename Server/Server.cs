8ff986498346335356bf9a0d701e1e62cb8fc4a6
ForeverFast <59344813+ForeverFast@users.noreply.github.com>
2019-12-30 20:57:28 +0300

    Kek

--- Server/Server.cs
+++ Server/Server.cs
@@ -32,6 +32,7 @@ using System.Net;
 using System.Net.Sockets;
 
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -65,7 +66,7 @@ namespace Server
             //
 
             var exePath = AppDomain.CurrentDomain.BaseDirectory; // Путь до .exe файла.
-            var path = System.IO.Path.Combine(exePath, "FT.db"); // Путь до .exe файла + название БД
+            var path = System.IO.Path.Combine(exePath, "ChSV.db"); // Путь до .exe файла + название БД
             constr = $"Data Source={path};Version=3;"; // Строка подключения к БД
 
             // Подключение к БД
@@ -80,7 +81,7 @@ namespace Server
         //
         //  Код, отвечающий за запуск сервера и его работу (2.1)
         //
-
+        Thread thread;
         private void StartServer_Click(object sender, EventArgs e)
         {
             // Смена названия кнопки
@@ -92,113 +93,138 @@ namespace Server
             // Запуск/Закрытие сервера
             if (CheckW == false)
             {
-                bw = new BackgroundWorker(); 
-                bw.WorkerSupportsCancellation = true;
-                bw.DoWork += (obj, ea) => Listner(); // Метод прослушки порта 8008
-                bw.RunWorkerAsync(); // Открытие сервера
+                //bw = new BackgroundWorker();
+                //bw.WorkerSupportsCancellation = true;
+                //bw.DoWork += (obj, ea) => Listner(); // Метод прослушки порта 8008
+                //bw.RunWorkerAsync(); // Открытие сервера
+
+                thread = new Thread( () =>
+                {
+                    //получаем адреса для запуска сокета
+                    IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(server), port);
+
+                    //создаем сокет
+                    Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    try
+                    {
+                         
+                        //связываем сокет с локальной точкой, по которой будем принимать данные
+                        listenSocket.Bind(iPEndPoint);
+
+                        //начинам прослушивание
+                        listenSocket.Listen(10);
+
+                        Console.WriteLine("Сервер работает");
+
+                        while (true)
+                        {
+                            Socket handler = listenSocket.Accept();
+
+                            //получаем сообщение
+                            StringBuilder builder = new StringBuilder();
+                            int bytes = 0; //кол-во полученых байтов
+                            byte[] data = new byte[256];
+                            do
+                            {
+                                bytes = handler.Receive(data);
+                                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                            } while (handler.Available > 0);
+                            string str = builder.ToString(); // Итоговая строка
+
+                            // Разделяем str на слова
+                            string[] lp = str.Split(' ');
+
+                            // Ветвление кода на основе типа запроса из lp[0]
+                            // lp[0] - первое слово в str, означающее тип запроса
+                            switch (lp[0])
+                            {
+                                case "LoginZ": // Авторизация
+                                    string[] ServAnsw = new string[2];
+                                    ServAnsw = CheckUser(lp[1], lp[2]);
+
+                                    // отправляем ответ
+                                    if (ServAnsw[0] == "true")
+                                    {
+                                        data = Encoding.Unicode.GetBytes((ServAnsw[0] + ServAnsw[1]).ToString());
+                                        handler.Send(data);
+                                        StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Успешное подключение: " + handler.LocalEndPoint.ToString();
+                                    }
+                                    else
+                                    {
+                                        data = Encoding.Unicode.GetBytes((ServAnsw[0] + ServAnsw[1]).ToString());
+                                        handler.Send(data);
+                                        StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Отклонено: " + handler.LocalEndPoint.ToString();
+                                    }
+
+                                    break;
+                                case "FileZ":
+
+
+
+
+
+                                    break;
+                                case "PingZ":
+                                    StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Пинг от: " + handler.LocalEndPoint.ToString();
+
+                                    break;
+                                default:
+                                    data = Encoding.Unicode.GetBytes("error");
+                                    handler.Send(data);
+                                    StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Отправлено сообщение об ошибке: " + handler.LocalEndPoint.ToString();
+                                    break;
+                            }
+
+
+
+
+
+
+
+
+                            // закрываем сокет
+                            handler.Shutdown(SocketShutdown.Both);
+                            handler.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+                thread.IsBackground = false;
+                thread.Start();
+
                 StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Сервер запущен";
                 CheckW = true;
             }
             else
             {
-                bw.CancelAsync(); // Закрытие сервера
+                //if (listenSocket != null)
+                //    listenSocket.Close();
+                //if (handler != null)
+                //    if (handler.Connected)
+                //    {
+                //        handler.Shutdown(SocketShutdown.Both);
+                //        handler.Close();
+                //    }
+                //bw.CancelAsync(); // Закрытие сервера
+
+                if(thread.IsAlive)
+                    thread.Abort();
+
                 StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Сервер закрыт";
                 CheckW = false;
             }
         }
 
+
+       
+
         void Listner()
         {
-            //получаем адреса для запуска сокета
-            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(server), port);
-
-            //создаем сокет
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            try
-            {
-
-                //связываем сокет с локальной точкой, по которой будем принимать данные
-                listenSocket.Bind(iPEndPoint);
-
-                //начинам прослушивание
-                listenSocket.Listen(10);
-
-                Console.WriteLine("Сервер работает");
-
-                while (!bw.CancellationPending)
-                {
-                    Socket handler = listenSocket.Accept();
-
-                    //получаем сообщение
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0; //кол-во полученых байтов
-                    byte[] data = new byte[256];
-                    do
-                    {
-                        bytes = handler.Receive(data);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    } while (handler.Available > 0);
-                    string str = builder.ToString(); // Итоговая строка
-
-                    // Разделяем str на слова
-                    string[] lp = str.Split(' ');
-                    
-                    // Ветвление кода на основе типа запроса из lp[0]
-                    // lp[0] - первое слово в str, означающее тип запроса
-                    switch (lp[0])
-                    {
-                        case "LoginZ": // Авторизация
-                            string[] ServAnsw = new string[2];
-                            ServAnsw = CheckUser(lp[1], lp[2]);
-
-                            // отправляем ответ
-                            if (ServAnsw[0] == "true")
-                            {
-                                data = Encoding.Unicode.GetBytes((ServAnsw[0] + ServAnsw[1]).ToString());
-                                handler.Send(data);
-                                StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Успешное подключение: " + handler.LocalEndPoint.ToString();
-                            }
-                            else
-                            {
-                                data = Encoding.Unicode.GetBytes((ServAnsw[0] + ServAnsw[1]).ToString());
-                                handler.Send(data);
-                                StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Отклонено: " + handler.LocalEndPoint.ToString();
-                            }
-
-                            break;
-                        case "FileZ":
-
-
-
-
-
-                            break;
-
-                        default:
-                            data = Encoding.Unicode.GetBytes("error");
-                            handler.Send(data);
-                            StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Отправлено сообщение об ошибке: " + handler.LocalEndPoint.ToString();
-                            break;
-                    }
-
-
-                    //Делим строку на login и pass
-
-
-                   
-                    
-
-
-                    // закрываем сокет
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            
         }
 
         // Метод для определения правильности введённых данных для входа
@@ -208,7 +234,7 @@ namespace Server
 
             // Поиск логина/пароля
             CMD = DB.CreateCommand();
-            CMD.CommandText = "SELECT * FROM user;";
+            CMD.CommandText = "SELECT * FROM users_ChSV;";
             SQL = CMD.ExecuteReader();
             if (SQL.HasRows)
             {
@@ -218,7 +244,7 @@ namespace Server
                     if (SQL["Login"].ToString() == login && SQL["Password"].ToString() != pass)
                     {
                         str[0] = "false";
-                        str[1] = "Пользователя с таким логином - не существует";
+                        str[1] = "Неправильный пароль";
                         StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Соединение отклонено";
                         SQL.Close();
                         return str;
@@ -248,6 +274,10 @@ namespace Server
                 SQL.Close();
                 return str;
             }
+            str[0] = "false";
+            str[1] = "Пользователя с таким логином - не существует";
+            StatusBox.Text += "\r\n(" + DateTime.Now.ToString() + ") Соединение отклонено ";
+            SQL.Close();
             return str;
         }
 
